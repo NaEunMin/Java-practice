70dab0b1efbd3f2326cad810b997e8a4e87e0ee2
NaEunMin <123241668+NaEunMin@users.noreply.github.com>
2024-10-06 14:53:33 +0900

    Create 5장1번.java

---
+++ 5장연습문제/5장1번.java
@@ -0,0 +1,26 @@
+class TV{
+	private int size;
+	public TV(int size) {this.size = size;}
+	protected int getSize() {
+		return size;
+	}
+}
+class ColorTV extends TV{
+	private int color;
+	public ColorTV(int size, int color) {
+		super(size);
+		this.color = color;
+	}
+	public void printProperty() {
+		System.out.println(getSize() + "인치 " + this.color + "컬러");
+	}
+}
+public class StudyEx {
+
+	public static void main(String[] args) {
+		ColorTV myTv = new ColorTV(65, 65536);
+		myTv.printProperty();
+
+	}
+
+}
